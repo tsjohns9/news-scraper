c28360859e1c501d8b364ebd7da3e2b6c114b749
Trevor Johnson <johnsontrevor55@gmail.com>
2018-06-05 21:22:50 -0700

    added authentication to the routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,6 +5,11 @@ const db = require('../models');
 
 router.get('/', (req, res) => {
   const obj = {};
+  if (req.isAuthenticated()) {
+    obj.auth = true;
+  } else {
+    obj.auth = null;
+  }
   obj.page = '/';
   obj.success = req.session.success;
 
@@ -22,30 +27,48 @@ router.get('/', (req, res) => {
 // loads saved articles for the user
 router.get('/saved', (req, res) => {
   const obj = {};
-  obj.page = '/saved';
-  obj.articles = [];
-
-  res.render('saved.hbs', obj);
+  if (req.isAuthenticated()) {
+    obj.auth = true;
+    obj.page = '/saved';
+    obj.articles = [];
+    res.render('saved.hbs', obj);
+  } else {
+    obj.auth = null;
+    res.send('404');
+  }
 });
 
 // signup page
 router.get('/signup', (req, res) => {
-  const obj = {};
-  obj.page = '/signup';
-  obj.errors = req.session.errors;
+  if (!req.isAuthenticated()) {
+    const obj = {};
+    obj.page = '/signup';
+    obj.errors = req.session.errors;
 
-  res.render('signup.hbs', obj);
-  req.session.errors = null;
+    res.render('signup.hbs', obj);
+    req.session.errors = null;
+  }
+  res.send('404');
 });
 
 // login page
 router.get('/login', (req, res) => {
-  const obj = {};
-  obj.page = '/login';
-  obj.errors = req.session.errors;
+  if (!req.isAuthenticated()) {
+    const obj = {};
+    obj.page = '/login';
+    obj.errors = req.session.errors;
 
-  res.render('login.hbs', obj);
-  req.session.errors = null;
+    res.render('login.hbs', obj);
+    req.session.errors = null;
+  }
+  res.send('404');
+});
+
+router.get('/logout', (req, res) => {
+  if (req.isAuthenticated()) {
+    req.logout();
+  }
+  res.redirect('/');
 });
 
 module.exports = router;
